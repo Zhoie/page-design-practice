bb11c74e18c6e0dba246fa1050386a0ce107188d
Niklas <zhoie@icloud.com>
2023-04-21 18:20:28 +0800

    Fix:user card css.

--- src/components/financePage/Hero.tsx
+++ src/components/financePage/Hero.tsx
@@ -3,7 +3,7 @@ import Typewriter from './Typewriter'
 
 
 const styles = {
-    container: 'flex flex-col mx-4 items-center pb-6 justify-center text-white',
+    container: 'flex flex-col h-screen mx-4 items-center pb-6 justify-center text-white',
     textAnimate: 'text-6xl sm:text-8xl p-4 text-transparent bg-clip-text font-bold bg-gradient-to-r from-teal-500 to-teal-50',
     textPulse: 'text-2xl text-white font-bold animate-pulse',
     startBtn: ' px-4 py-2  my-4 bg-teal-400 text-zinc-950 text-sm sm:text-xl font-bold rounded-xl hover:text-teal-500 hover:bg-zinc-950 hover:ring-2 hover:ring-teal-500 transition ease-in-out duration-300',
