0d6e056c27e4eecdd653b3a1968251279cafdcc7
Niklas <zhoie@icloud.com>
2023-05-10 17:50:28 +0800

    fix:data shows.

---
+++ src/app/framer/motions.ts
@@ -0,0 +1,15 @@
+const navbarVariants = {
+    hidden: {
+        y: '-100%',
+        opacity: 0,
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 0.5,
+            type: 'spring',
+        },
+    },
+};
