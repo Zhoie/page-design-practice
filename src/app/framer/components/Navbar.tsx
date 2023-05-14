301af175faca09bfb29d6938e4f9396dc4f64f59
Niklas <zhoie@icloud.com>
2023-05-14 16:31:18 +0800

    Add: light button.

--- src/app/framer/components/Navbar.tsx
+++ src/app/framer/components/Navbar.tsx
@@ -1,13 +1,17 @@
 'use client'
 import React from 'react'
 import styles from './Navbar.module.css'
+import {motion} from 'framer-motion'
+import Block from './Block'
+import { animations } from '../motions'
 
 
 export default function Navbar() {
     return (
-        <div className={styles.navbar} >
+        <motion.div className={styles.navbar} variants={animations} initial={'initial'} animate={'show'} whileHover={'hover'} >
             <p>Motions</p>
+            {/* <Block /> */}
             
-        </div>
+        </motion.div>
     )
 }
